2df205fb4a9e7431e0465acb4860140f1fbe9de8
Lillian Parker <Lillianpparker@outlook.com>
2020-09-05 16:06:18 -0500

    SnakeList.js completed

---
+++ src/components/SnakeList/SnakeList.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import SnakeShow from "../SnakeShow/SnakeShow";
+
+const SnakeList = (props) => {
+    const [snakes, setSnakes] = useState([]);
+
+    useEffect(() => {
+        ( async () => {
+            try {
+                const response = await fetch('/api');
+                const data = await response.json();
+                updateAllSnakes([...data]);
+            } catch (e) {
+                console.log(e)
+            }
+        })();
+    }, []);
+
+const showSnake = snakes.map((snake, i) => {
+    return (
+        <div key={i}>
+            <SnakeShow snake={snake} isLoggedIn={props.isLoggedIn}/>
+        </div>
+    );
+});
+    return <div>{showSnake}</div>
+};
+
+export default SnakeList;
