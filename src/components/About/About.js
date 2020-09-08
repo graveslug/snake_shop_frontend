da9423e60cab0877a00484aabeb71117c0af70c4
Lillian Parker <Lillianpparker@outlook.com>
2020-09-08 17:45:27 -0500

    most of about me  css done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,30 +1,36 @@
 import React from 'react';
-
+import "./About.css"
 
 
 function About (props){
         return(
-            <div>
+            <div className="background">
                 <div>
-                    <h1>About Us</h1>
+                    <h2 className="title">About Us</h2>
                     <p>
                         Welcome to Hissery. We are a group of classmates deciding to give you the best experience with look and reserving snakes out there. Philip Gray, Lillian Parker, and Isaac Dennis hope you enjoy this webpage and get a snake of your dreams.
                     </p>
                 </div>
                 <div>
-                    <h1>About Website</h1>
+                    <h2 className="title">About Website</h2>
                     <p>
                         Here on Hissery, we provide high quality snakes where you can breed particular snakes to your liking. Through our reservation system you can reserve the snake(s). We have a wide variety of snakes to choose from. 
                     </p>
                 </div>
-                <div>
-                    <h1>Contact Us </h1>
-                    <h2>Philip Gray</h2>
+                <h2>Contact Us </h2>
+                <div className="allContacts">
+                    <div className="section">
+                    <h3  className="contact">Philip Gray</h3>
                     <p>Email: philip.gray@email.com</p>
-                    <h2>Isaac Dennis</h2>
-                    <p>Email:isaac.dennis@email.com</p>
-                    <h2>Lillian Parker</h2>
+                    </div><br/>
+                    <div className="section">
+                    <h3  className="contact">Isaac Dennis</h3>
+                    <p>Email: isaac.dennis@email.com</p>
+                    </div><br/>
+                    <div className="section">
+                    <h3  className="contact">Lillian Parker</h3>
                     <p>Email: lillian.parker@email.com</p>
+                    </div>
                 </div>
             </div>
         )
