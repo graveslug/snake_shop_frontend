655578ccda8cb2d01a1ed6c97d318cc95d864d22
Philip Gray <pcgrayiii@icloud.com>
2020-09-09 18:10:58 -0700

    build for githubpages

---
+++ build/precache-manifest.edce7d88467d90e3460a4ad6d1471430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0e30a6eec41cfbc7fdbef57f3ffd9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a1802d56d4ca67eac48",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6e2abc64e64296e2ea4",
+    "url": "/static/css/main.51afdaea.chunk.css"
+  },
+  {
+    "revision": "1a1802d56d4ca67eac48",
+    "url": "/static/js/2.b6cb60bb.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.b6cb60bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e2abc64e64296e2ea4",
+    "url": "/static/js/main.977d59d5.chunk.js"
+  },
+  {
+    "revision": "95d0f28da12feee38652",
+    "url": "/static/js/runtime-main.cf904017.js"
+  }
+]);
